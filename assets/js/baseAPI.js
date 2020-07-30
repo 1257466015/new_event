227fc9364bbee975b0f19cf918b154a58f6e0474
王嘉城 <1257466015@qq.com>
2020-07-30 18:58:30 +0800

    完成了个人中心更换头像的功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,23 +1,23 @@
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax()的时候，
 // 会先调用ajaxPrefilter这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象，拦截每一次ajax请求
-$.ajaxPrefilter(function (optins) {
+$.ajaxPrefilter(function (options) {
     //z 在准备发起真正的Ajax请求之前，统一拼接请求的根路径
-    optins.url = 'http://ajax.frontend.itheima.net' + optins.url;
+    options.url = 'http://ajax.frontend.itheima.net' + options.url;
     // console.log(optins.url);
 
-    // optins.headers = {
+    // options.headers = {
     //     Authorization: localStorage.getItem('token') || ''
     // }
 
-    if (optins.url.indexOf('/my/') !== -1) {
-        optins.headers = {
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
             Authorization: localStorage.getItem('token') || ''
         }
     }
 
     // 全局统一挂载complete函数
-    optins.complete = function (res) {
+    options.complete = function (res) {
         if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
             // 强制清空 token
             localStorage.removeItem('token')
